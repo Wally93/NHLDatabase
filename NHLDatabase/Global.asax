a39cd9d8f69980c8970f2d7fae0476b53ab7ae13
Wally Jniyah <ojniyah@gmail.com>
2017-07-20 15:50:46 -0400

    Add project files.

---
+++ NHLDatabase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NHLDatabase.MvcApplication" Language="C#" %>
---
+++ NHLDatabase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NHLDatabase.Startup))]
+namespace NHLDatabase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
